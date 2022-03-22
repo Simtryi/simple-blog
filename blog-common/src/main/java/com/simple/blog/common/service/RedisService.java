2470c51fe279538f3df08968ebb87e0038c753cf
simple <2579115705@qq.com>
2022-03-22 19:59:48 +0800

    Add redis service.

---
+++ blog-common/src/main/java/com/simple/blog/common/service/RedisService.java
@@ -0,0 +1,55 @@
+package com.simple.blog.common.service;
+
+import java.util.List;
+
+/**
+ * Redis相关操作
+ */
+public interface RedisService {
+
+    /**
+     * 保存属性
+     */
+    void set(String key, Object value, long time);
+
+    /**
+     * 保存属性
+     */
+    void set(String key, Object value);
+
+    /**
+     * 获取属性
+     */
+    Object get(String key);
+
+    /**
+     * 删除属性
+     */
+    Boolean del(String key);
+
+    /**
+     * 批量删除属性
+     */
+    Long del(List<String> keys);
+
+    /**
+     * 设置过期时间
+     */
+    Boolean expire(String key, long time);
+
+    /**
+     * 获取过期时间
+     */
+    Long getExpire(String key);
+
+    /**
+     * 递增
+     */
+    Long incr(String key, long delta);
+
+    /**
+     * 递减
+     */
+    Long decr(String key, long delta);
+    
+}
